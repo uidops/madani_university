67607941117df96bd92f9471743db3412c76e7b9
uidops <uidops@protonmail.com>
2022-11-21 19:05:11 +0330

    update khorablo/04

--- khorablo/04/main.py
+++ khorablo/04/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-l1 = [0] + list(map(int, input().split()))
-l2 = [0] + list(map(int, input().split()))
-l3 = [0] + list(map(int, input().split()))
-l = [l1, l2, l3, [0]*4]
+l1 = list(map(int, input().split())) + [0]
+l2 = list(map(int, input().split())) + [0]
+l3 = list(map(int, input().split())) + [0]
+l = [l1, l2, l3, [0]*len(l1)]
 
 print()
-for irow in range(3):
-    for ielem in range(1, 4):
-        print(l[irow+1][ielem] + l[irow-1][ielem] + l[irow][(ielem+1)&3] + l[irow][(ielem-1)&3], end=' ')
+for irow in range(len(l)-1):
+    for icol in range(len(l[0])-1):
+        print(l[irow+1][icol] + l[irow-1][icol] + l[irow][icol+1] + l[irow][icol-1], end=' ')
 
     print()
