24f996ea6b2c68a0924b0007cd3f2be8811e941e
uidops <uidops@protonmail.com>
2022-11-25 18:02:11 +0330

    update final project: parse with pure integer instead of str

--- final/main.py
+++ final/main.py
@@ -1,89 +1,90 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import math
 import sys
 
-numbers = ('صفر', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه',
-        'ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده')
 
-excep_scales = ('بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود',
-        'صد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد')
+numbers = ['صفر', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه',
+        'ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده']
 
-short_scales = ('هزار', 'میلیون', 'بیلیون', 'تریلیون', 'کوآدریلیون', 'کوینتیلیون', 'سکستیلیون',
+excep_scales = ['بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود',
+        'صد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد']
+
+short_scales = ['هزار', 'میلیون', 'بیلیون', 'تریلیون', 'کوآدریلیون', 'کوینتیلیون', 'سکستیلیون',
         'سپتیلیون', 'اکتیلیون', 'نانیلیون', 'دسیلیون', 'آندسیلیون', 'دیودسیلیون',
         'تریدسیلیون', 'کواتیوردسیلیون', 'کویندسیلیون', 'سکسدسیلیون', 'سپتدسیلیون',
         'اُکتودسیلیون', 'نومدسیلیون', 'ویجینتیلیون', 'آنویجینتیلیون', 'دویجینتیلیون', 'ترسویجینتیلیون',
         'کوادرویجینتیلیون', 'کوینکاویجینتیلیون', 'سیسویجینتیلیون', 'سپتمویجینتیلیون', 'آکتوویجینتیلیون',
-        'نومویجینتیلیون', 'تریویجینتیلیون', 'آنتریویجینتیلیون', 'دوتریویجینتیلیون', 'گوگول')
+        'نومویجینتیلیون', 'تریویجینتیلیون', 'آنتریویجینتیلیون', 'دوتریویجینتیلیون', 'گوگول']
 
-separator = ' و '
-currency = ' ریال'
+
+separator = 'و'
+__currency = 'ریال'
 negative = 'منفی '
 
 
-def main():
+def __main():
     if len(sys.argv) != 2:
         print(f'Usage: {sys.argv[0]} [num]')
         sys.exit(1)
 
-    number = sys.argv[1].strip() + '0'
-    try:
+    number = sys.argv[1].strip()# + '0'
+    if number.isdigit() or (number[0] in ('+', '-') and number[1:].isdigit()):
         number = int(number)
-    except ValueError:
+
+    else:
         print('The input is not an integer')
         sys.exit(1)
 
-    print(number_to_text(number) + currency)
+    print(number_to_text(number) + ' ' + __currency)
 
 
 def number_to_text(number=0, text='', flag=0):
-    if type(number) != int:
+    if type(number) != int or not (bool(text) or number):
         return numbers[0]
 
-    number = str(number)
-    if number[0] == '-':
-        number = number[1:]
-        text += negative
-
-    ret = None
-    if not len(number) or number == '0':
-        if not bool(text):
-            text += numbers[0]
-
+    elif number == 0:
         return text
 
-    if flag and set(number) != {'0'}:
-        text += separator
 
-    if len(number) == 1:
-        text += numbers[int(number[0])]
+    if number < 0:
+        number = abs(number)
+        text += negative
 
-    elif len(number) == 2:
-        if number[0] == '1':
-            text += numbers[int(number[1]) + 10]
+    length = math.floor(math.log10(number)) + 1
+
+    ret = None
+    if flag:
+        text += ' ' + separator + ' '
+
+    if length == 1:
+        text += numbers[number//(10**(length-1))]
+
+    elif length == 2:
+        if number//(10**(length-1)) == 1:
+            text += numbers[(number//(10**(length-2)))%10 + 10]
 
         else:
-            text += excep_scales[int(number[0]) - 2]
+            text += excep_scales[number//(10**(length-1)) - 2]
             ret = 1
 
-    elif len(number) == 3:
-        if number[0] != '0':
-            text += excep_scales[int(number[0]) + 7]
+    elif length == 3:
+        if number//(10**(length-1)) != '0':
+            text += excep_scales[number//(10**(length-1)) + 7]
 
         ret = 1
 
     else:
-        n = len(number)%3
-        n += (n+3)*(not n)
+        n = length%3 + (length%3+3)*(not length%3)
 
-        text = number_to_text(int(number[:n]), text)
-        if number[0] != '0':
-            text += ' ' + short_scales[int(-(-(len(number)/3)//1)) - 2]
+        text = number_to_text(number//(10**(length-n)), text)
+        text += ' ' + short_scales[math.trunc(-(-(length/3)//1)) - 2]
 
         ret = n
 
-    return text if ret == None else number_to_text(int(number[ret:]), text, 1)
+    return text if ret == None else number_to_text(number%(10**(length-ret)), text, 1)
 
 
 if __name__ == '__main__':
-    main()
+    __main()
