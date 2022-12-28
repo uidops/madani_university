f9a611667eccc494619dc40e0c5bdc973d137094
uidops <uidops@protonmail.com>
2022-12-28 14:50:44 +0330

    update 08

--- 08/main.py
+++ 08/main.py
@@ -9,12 +9,12 @@ def rot(s: str, n=1, k='a:d/fc') -> str:
     s = list(s)
     for i in range(len(s)):
         if s[i].isupper():
-            s[i] = chr(65+((ord(s[i])-65+n)%26))
+            s[i] = chr(0x40|(((ord(s[i])^0x40)+n)%0x1a or 0x1a))
         elif s[i].islower():
-            s[i] = chr(97+((ord(s[i])-97+n)%26))
+            s[i] = chr(0x60|(((ord(s[i])^0x60)+n)%0x1a or 0x1a))
 
     return k+''.join(s)
 
 
-print(rot(input('text to enctypt: ')))
-print(rot(input('text to decrypt: ')))
+print(rot(input('text to enctypt: '), 1))
+print(rot(input('text to decrypt: '), 1))
